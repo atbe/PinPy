1755791f2245302349726ea3b0db9685c1ddc3f0
@be <abeahmed2@gmail.com>
2017-05-16 20:27:13 -0400

    Fixed some issues with unknown error code exceptions.

--- pypin/exceptions.py
+++ pypin/exceptions.py
@@ -1,5 +1,5 @@
 class PyPinException(Exception):
-    def __init__(self, response):
+    def __init__(self, response=None):
         self.response = response
 
 class PyPinContentNotFoundError(PyPinException):
@@ -7,7 +7,16 @@ class PyPinContentNotFoundError(PyPinException):
     def __str__(self):
         return '404 response from Pinterest. Content not found.'
 
-class PyPinUnhandledResponseCodeError(Exception):
+    def __repr__(self):
+        return self.__str__()
+
+class PyPinUnhandledResponseCodeError(PyPinException):
+
+    def __init__(self, response=None):
+        super().__init__(response)
 
     def __str__(self):
-        return 'Unhandled response code returned from Pinterest.'
+        return 'Unhandled response code returned from Pinterest = {}'.format(self.response.status_code)
+
+    def __repr__(self):
+        return "{}:\nStatus Code={}".format(self.__str__str(), self.response.status_code)
