a712295eef601b669ef95208d809951cd966467c
@be <abeahmed2@gmail.com>
2018-06-26 13:40:15 -0400

    Corrected version number (still in pre-release).

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pinpy',
-      version='0.1',
+      version='0.0.1',
       description='Python client for Pinterest API',
       author='Ibrahim Ahmed',
       author_email='abeahmed2@gmail.com',
